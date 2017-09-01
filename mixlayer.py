c7d1b8f4abb50ec521773cc04a9a104cf37f21b1
Ashwin Srinath <ashwinsrnth@gmail.com>
2017-09-01 05:36:19 -0400

    Try porting to numba

--- mixlayer.py
+++ mixlayer.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import yaml
-
+from numba import jit, float64
 
 def eos(rho, rho_u, rho_v, egy, tmp, prs, Cv, Rspecific):
     tmp[:, :] = (egy - 0.5*(rho_u**2 + rho_v**2)/rho)/(rho*Cv)
@@ -37,7 +37,7 @@ def calculate_timestep(x, y, rho, rho_u, rho_v, tmp,
 
 def rhs_euler_terms(rho, rho_u, rho_v, egy, rho_rhs, rho_u_rhs,
         rho_v_rhs, egy_rhs, prs, dx, dn, dndy):
-    
+ 
     rho_rhs[...] = -dfdy(rho_v, dn)*dndy
     rho_rhs[[0,-1], :] = 0
     rho_rhs[...] += -dfdx(rho_u, dx)
@@ -179,29 +179,50 @@ def non_reflecting_boundary_conditions(rho, rho_u, rho_v, egy, rho_rhs, rho_u_rh
     for rhs in rho_rhs, rho_u_rhs, rho_v_rhs, egy_rhs:
         apply_boundary_filter(rhs, filter_amplitude)
 
+@jit(nopython=True)
 def dfdx(f, dx):
-    dfdx = (1./dx)*((4./5)*(np.roll(f,-1,1)-np.roll(f,+1,1)) + 
-            (-1./5)*(np.roll(f,-2,1)-np.roll(f,+2,1)) +
-            (4./105)*(np.roll(f,-3,1)-np.roll(f,+3,1)) +
-            (-1./280)*(np.roll(f,-4,1)-np.roll(f,+4,1)))
-    return dfdx
- 
-def dfdy(f, dy):
-    N = f.shape[0]
-    dfdy =  (1./dy)*((4./5)*(np.roll(f,-1,0)-np.roll(f,+1,0)) + 
-            (-1./5)*(np.roll(f,-2,0)-np.roll(f,+2,0)) +
-            (4./105)*(np.roll(f,-3,0)-np.roll(f,+3,0)) +
-            (-1./280)*(np.roll(f,-4,0)-np.roll(f,+4,0)))
-    dfdy[0, :] = (-11*f[0,:]+18*f[1,:]-9*f[2,:]+2*f[3,:])/(6.*dy)
-    dfdy[1, :] = (-2*f[0,:]-3*f[1,:]+6*f[2,:]-1*f[3,:])/(6.*dy)
-    dfdy[2, :] = 2*(f[3,:]-f[1,:])/(3*dy) - 1*(f[4,:]-f[0,:])/(12*dy)
-    dfdy[3, :] = 3*(f[4,:]-f[2,:])/(4*dy) - 3*(f[5,:]-f[1,:])/(20*dy) + 1*(f[6,:]-f[0,:])/(60*dy) 
+    out = np.empty_like(f, dtype=np.float64)
+    ny, nx = f.shape
+    for i in range(ny):
+        for j in range(4):
+            out[i,j] = (1./dx)*((4./5)*(f[i,j+1] - f[i,j-1]) + 
+                    (-1./5)*(f[i,j+2] - f[i,j-2]) +
+                    (4./105)*(f[i,j+3] - f[i,j-3]) +
+                    (-1./280)*(f[i,j+4] - f[i,j-4]))
 
-    dfdy[-1, :] = -((-11*f[-1,:]+18*f[-2,:]-9*f[-3,:]+2*f[-4,:])/(6.*dy))
-    dfdy[-2, :] = -((-2*f[-1,:]-3*f[-2,:]+6*f[-3,:]-1*f[-4,:])/(6.*dy))
-    dfdy[-3, :] = -(2*(f[-4,:]-f[-2,:])/(3*dy) - 1*(f[-5,:]-f[-1,:])/(12*dy))
-    dfdy[-4, :] = -(3*(f[-5,:]-f[-3,:])/(4*dy) - 3*(f[-6,:]-f[-2,:])/(20*dy) + 1*(f[-7,:]-f[-1,:])/(60*dy))
-    return dfdy
+        for j in range(4, nx-4):
+            out[i,j] = (1./dx)*((4./5)*(f[i,j+1] - f[i,j-1]) + 
+                    (-1./5)*(f[i,j+2] - f[i,j-2]) +
+                    (4./105)*(f[i,j+3] - f[i,j-3]) +
+                    (-1./280)*(f[i,j+4] - f[i,j-4]))
+            
+        for j in range(-1, -5, -1):
+            out[i,j] = (1./dx)*((4./5)*(f[i,j+1] - f[i,-1]) + 
+                    (-1./5)*(f[i,2] - f[i,-2]) +
+                    (4./105)*(f[i,3] - f[i,-3]) +
+                    (-1./280)*(f[i,+4] - f[i,-4]))
+    return out
+
+@jit(nopython=True)
+def dfdy(f, dy):
+    ny, nx = f.shape
+    out = np.empty_like(f, dtype=np.float64)
+    for i in range(4, ny-4):
+        for j in range(nx):
+            out[i,j] =  (1./dy)*((4./5)*(f[i-1,j] - f[i+1,j]) +
+                    (-1./5)*(f[i-2,j] - f[i+2,j]) +
+                    (4./105)*(f[i-3,j] - f[i+3,j]) +
+                    (-1./280)*(f[i-4,j] - f[i+4,j]))
+        out[0,j] = (-11*f[0,j]+18*f[1,j]-9*f[2,j]+2*f[3,j])/(6.*dy)
+        out[1,j] = (-2*f[0,j]-3*f[1,j]+6*f[2,j]-1*f[3,j])/(6.*dy)
+        out[2,j] = 2*(f[3,j]-f[1,j])/(3*dy) - 1*(f[4,j]-f[0,j])/(12*dy)
+        out[3,j] = 3*(f[4,j]-f[2,j])/(4*dy) - 3*(f[5,j]-f[1,j])/(20*dy) + 1*(f[6,j]-f[0,j])/(60*dy) 
+
+        out[-1,j] = -((-11*f[-1,j]+18*f[-2,j]-9*f[-3,j]+2*f[-4,j])/(6.*dy))
+        out[-2,j] = -((-2*f[-1,j]-3*f[-2,j]+6*f[-3,j]-1*f[-4,j])/(6.*dy))
+        out[-3,j] = -(2*(f[-4,j]-f[-2,j])/(3*dy) - 1*(f[-5,j]-f[-1,j])/(12*dy))
+        out[-4,j] = -(3*(f[-5,j]-f[-3,j])/(4*dy) - 3*(f[-6,j]-f[-2,j])/(20*dy) + 1*(f[-7,j]-f[-1,j])/(60*dy))
+    return out
     
 if __name__ == "__main__":
     # read parameters
@@ -303,7 +324,6 @@ if __name__ == "__main__":
 
 
     for i in range(50000):
-        print(i)
         eos(rho, rho_u, rho_v, egy, tmp, prs, Cv, Rspecific)
 
         dt = calculate_timestep(x, y, rho, rho_u, rho_v, tmp, gamma_ref, mu_ref, kappa_ref,
@@ -336,4 +356,3 @@ if __name__ == "__main__":
             plt.yticks([])
             plt.savefig("{:05d}.png".format(i))
             plt.close()
-
