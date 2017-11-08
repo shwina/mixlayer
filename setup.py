969990bba9b191642daa2c760061519e99d1a749
Ashwin Srinath <ashwinsrnth@gmail.com>
2017-11-08 04:31:03 -0500

    Directory structure renaming

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mixlayer',
+    version='0.1',
+    description='A finite-difference code for analyzing mixing layer flow',
+    url='github.com/shwina/mixlayer',
+    author='Ashwin Srinath',
+    packages=['mixlayer'],
+    zip_safe=False)
