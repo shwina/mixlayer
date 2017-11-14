9e1a6f9673decdd168b4575294ea762a071eac07
Ashwin Srinath <atrikut@clemson.edu>
2017-11-14 16:50:42 -0700

    Add simple tests for timestepper

---
+++ tests/test_timestepping.py
@@ -0,0 +1,30 @@
+from mixlayer.timestepping import RK4
+from numpy.testing import assert_equal, assert_allclose
+import numpy as np
+
+class TestRK4:
+
+    def setup(self):
+        fields = [np.ones(10, dtype=np.float64),
+                np.ones(10, dtype=np.float64)]
+
+        def rhs(fields):
+            return [np.ones(10, dtype=np.float64),
+                np.ones(10, dtype=np.float64)]
+
+        self.fields = fields
+        self.stepper = RK4(fields, rhs)
+
+    def test_take_step_zero_length(self):
+        self.stepper.step(0)
+        assert_allclose(self.fields[0], 1)
+        assert_allclose(self.fields[1], 1)
+
+    def test_take_one_step_and_compare_fields(self):
+        self.stepper.step(0.5)
+        assert_allclose(self.fields[0], self.fields[1])
+
+    def test_
+
+    
+        
