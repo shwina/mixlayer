9f9a477f35974e577e2976b8e7ad8bbc35cc7ded
Ashwin Srinath <atrikut@clemson.edu>
2018-01-31 18:01:21 -0500

    Add extra test for filtering

--- tests/test_filtering.py
+++ tests/test_filtering.py
@@ -12,3 +12,16 @@ def test_filter_ones_array():
     a = np.ones([10, 10], dtype=np.float64)
     filter5(a)
     assert_equal(a, 1)
+
+def test_filter_symmetric_array():
+    # symmetric about vertical line
+    a = np.random.rand(10, 5)
+    a = np.hstack([a, a[:, ::-1]])
+    filter5(a)
+    assert_equal(a[:, :5], a[:, -1:-6:-1])
+
+    # symmetric about horizontal line
+    a = np.random.rand(10, 5)
+    a = np.vstack([a, a[::-1, :]])
+    filter5(a)
+    assert_equal(a[:5, :], a[-1:-6:-1, :])
